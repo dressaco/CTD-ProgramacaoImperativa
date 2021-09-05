fd8681bc66ceb8af1be847e8a07afe34581004ff
Andressa Collin <dressaco@outlook.com>
2021-09-05 20:52:27 -0300

    Mini Desafio Aula 10

---
+++ aula10/miniDesafio.js
@@ -0,0 +1,24 @@
+let lista = ['caderno','lapiseira','borracha','caneta'];
+
+console.log(lista);
+
+console.log('O método join exibe a lista como uma string utilizando o separador especificado. Se não for atribuido um separador, o padrão será utilizar vírgula');
+console.log(lista.join(' '));
+console.log(lista);
+
+console.log('O método pop remove o último item da lista e exibe ele.')
+console.log(lista.pop());
+console.log(lista);
+
+console.log('O método push adiciona o item informado no final da lista.')
+console.log(lista.push('grafite'));
+console.log(lista);
+
+console.log('O método shift remove o primeiro item da lista e exibe ele.')
+console.log(lista.shift());
+console.log(lista);
+
+console.log('O método unshift adiciona o item informado no início da lista.')
+console.log(lista.unshift('marca-texto'));
+console.log(lista);
+
