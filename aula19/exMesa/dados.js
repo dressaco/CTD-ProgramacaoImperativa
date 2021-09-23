e7d9a323576df62aac4c1321ceee9308fcc38b04
Dressa Co <dressaco@outlook.com>
2021-09-22 22:40:51 -0300

    Atividade de Mesa Aula 19
    Co-authored-by: LuisaAndrade0607 <LuisaAndrade0607@users.noreply.github.com>
    Co-authored-by: ivanDourado <ivanDourado@users.noreply.github.com>
    Co-authored-by: alissonbtt <alissonbtt@users.noreply.github.com>
    Co-authored-by: Beatriz Malengo Andrade <BeatrizMalengo@users.noreply.github.com>

---
+++ aula19/exMesa/dados.js
@@ -0,0 +1,149 @@
+const dados = [
+    {
+      nome: "Maria",
+      sobrenome: "Azevedo",
+      idade: 83,
+      sexo: "feminino",
+      cidade: "Itabira",
+      peso: 76.5,
+      altura: 166,
+    },
+    {
+      nome: "Caio",
+      sobrenome: "Castro",
+      idade: 70,
+      sexo: "masculino",
+      cidade: "Várzea Paulista",
+      peso: 76.8,
+      
+      altura: 170
+    },
+    {
+      nome: "Kaua",
+      sobrenome: "Fernandes",
+      idade: 78,
+      sexo: "masculino",
+      cidade: "Teófilo Otoni",
+      peso: 66.4,
+      altura: 162
+    },
+    {
+      nome: "Julian",
+      sobrenome: "Costa",
+      idade: 72,
+      sexo: "masculino",
+      cidade: "Samambaia",
+      peso: 102.3,
+      altura: 163
+    },
+    {
+      nome: "Manuela",
+      sobrenome: "Cunha",
+      idade: 23,
+      sexo: "feminino",
+      cidade: "Taguatinga",
+      peso: 85.8,
+      altura: 174
+    },
+    {
+      nome: "Nicolas",
+      sobrenome: "Martins",
+      idade: 52,
+      sexo: "masculino",
+      cidade: "Osasco",
+      peso: 102.8,
+      altura: 170
+    },
+    {
+      nome: "Luiza",
+      sobrenome: "Martins",
+      idade: 79,
+      sexo: "feminino",
+      cidade: "Sousa",
+      peso: 105.7,
+      altura: 168
+    },
+    {
+      nome: "Luiza",
+      sobrenome: "Santos",
+      idade: 73,
+      sexo: "feminino",
+      cidade: "Mococa",
+      peso: 79.4,
+      altura: 158
+    },
+    {
+      nome: "Danilo",
+      sobrenome: "Sousa",
+      idade: 39,
+      sexo: "masculino",
+      cidade: "Betim",
+      peso: 95.3,
+      altura: 188
+    },
+    {
+      nome: "Giovanna",
+      sobrenome: "Goncalves",
+      idade: 77,
+      sexo: "feminino",
+      cidade: "Gravataí",
+      peso: 48.1,
+      altura: 167
+    },
+    {
+      nome: "Isabela",
+      sobrenome: "Costa",
+      idade: 59,
+      sexo: "feminino",
+      cidade: "Candeias",
+      peso: 99.1,
+      altura: 161
+    },
+    {
+      nome: "Luan",
+      sobrenome: "Rodrigues",
+      idade: 44,
+      sexo: "masculino",
+      cidade: "Samambaia",
+      peso: 104,
+      altura: 188
+    },
+    {
+      nome: "Gustavo",
+      sobrenome: "Rocha",
+      idade: 20,
+      sexo: "masculino",
+      cidade: "Cariacica",
+      peso: 66.6,
+      altura: 189
+    },
+    {
+      nome: "Martim",
+      sobrenome: "Costa",
+      idade: 41,
+      sexo: "masculino",
+      cidade: "Abreu e Lima",
+      peso: 88.6,
+      altura: 184
+    },
+    {
+      nome: "Vitoria",
+      sobrenome: "Correia",
+      idade: 34,
+      sexo: "feminino",
+      cidade: "Ribeirão Preto",
+      peso: 69,
+      altura: 158
+    },
+    {
+      nome: "Gabriela",
+      sobrenome: "Cunha",
+      idade: 30,
+      sexo: "feminino",
+      cidade: "Arcoverde",
+      peso: 87.9,
+      altura: 170
+    }
+]
+
+module.exports = dados;
---
+++ aula19/exMesa/readme.md
@@ -0,0 +1,8 @@
+## Atividade de Mesa - Aula 19
+
+Participantes:
+Alisson Bittecourt
+Andressa Malta
+Beatriz Andrade
+Ivan Felipe
+Luisa Andrade
